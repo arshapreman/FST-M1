ff90d63db80c7e7e3a2604cb2bf31c27e277fb69
arshapreman <106944549+arshapreman@users.noreply.github.com>
2026-01-19 12:28:48 +0530

    Add files via upload

---
+++ SQL/Activities/Activity3.sql
@@ -0,0 +1,9 @@
+---Activity3----
+Select salesman_id, salesman_city 
+From salesman;   
+
+Select salesman_name 
+from salesman where salesman_city = 'Paris'; 
+
+Select salesman_id, commission 
+from salesman where salesman_name = 'Paul Adam';
